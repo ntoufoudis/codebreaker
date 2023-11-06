47480fccf0d36bdbb7952f18f57426aa1ff9b71f
Vasileios Ntoufoudis <info@ntoufoudis.com>
2023-11-06 19:49:31 +0200

    Allow multiple lines

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -1,15 +1,18 @@
 @props(['message', 'letters'])
 
-<div {{ $attributes->class(['text-center flex gap-7 flex-wrap mt-auto']) }}>
-    @foreach(explode(' ', $message) as $word)
-        <div class="word flex gap-x-2">
-            @foreach(str_split($word) as $character)
-                <div>
-                    <span class="material-symbols-outlined character !text-4xl">
-                        {{ $letters[strtolower($character)] ?? $character }}
-                    </span>
-
-                    <div class="bg-gray-200 border border-gray-400 w-12 h-12"></div>
+<div {{ $attributes->class(['text-center flex gap-7 flex-wrap mt-auto flex-col']) }}>
+    @foreach(array_filter(explode("\n", trim($message))) as $line)
+        <div class="line flex flex-wrap gap-8">
+            @foreach(explode(' ', trim($line)) as $word)
+                <div class="word flex flex-wrap gap-x-2">
+                    @foreach(str_split($word) as $character)
+                        <div>
+                            <span class="material-symbols-outlined character !text-4xl">
+                                {{ $letters[strtolower($character)] ?? $character }}
+                            </span>
+                            <div class="bg-gray-200 border border-gray-400 w-12 h-12"></div>
+                        </div>
+                    @endforeach
                 </div>
             @endforeach
         </div>
